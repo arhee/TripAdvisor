ce2f8785221d2d3ef1927971b54c8967ac2b99b0
Alex Rhee <alex.rhee@gmail.com>
2015-10-13 10:25:10 -0400

    first

--- scraping/bin/sandbox.py
+++
@@ -1,5 +0,0 @@
-import time
-import calendar
-for i in range(10):
-    print('\r{}'.format(calendar.timegm(time.gmtime())))
-    time.sleep(1)
--- scraping/bin/teardown.py
+++
@@ -1,6 +0,0 @@
-import os
-import os.path
-if os.path.isfile('bookmark.json'):
-	os.remove('bookmark.json')
-if os.path.isfile('test.db'):
-	os.remove('trip_advisor.db')
